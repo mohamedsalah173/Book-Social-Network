a2dd83455c51d02f7b005cbf46e734d0c64007fe
mohamedsalah173 <mohamedsalahmasoud173@gmail.com>
2024-09-04 22:53:22 +0300

    create Role,User,Token Repositories and create token entity

---
+++ book-network/src/main/java/com/salah/book/user/Token.java
@@ -0,0 +1,28 @@
+package com.salah.book.user;
+
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDate createdAt;
+    private LocalDate expiresAt;
+    private LocalDate validatedAt;
+    @ManyToOne
+    @JoinColumn(name = "userId" , nullable = false)
+    private User user;
+}
