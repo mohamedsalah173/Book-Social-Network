f44684d43b1b244e96ec4994a652061061614183
mohamedsalah173 <mohamedsalahmasoud173@gmail.com>
2024-09-08 22:14:15 +0300

    create UserDetailsServiceImpl to implement UserDetailsService

---
+++ book-network/src/main/java/com/salah/book/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.salah.book.security;
+
+import com.salah.book.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository repository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findUserByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+    }
+}
